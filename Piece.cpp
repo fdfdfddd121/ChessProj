ded017897e77b1810c8f20cade7d1c4feea2870f
Nitay Karasso <nitaykaraso15@gmail.com>
2024-12-20 14:22:30 +0200

    added Piece.h and frame of Piece.cpp

---
+++ Piece.cpp
@@ -0,0 +1,41 @@
+#include "Piece.h"
+
+Piece::Piece(const std::string& place, const char type)
+{
+}
+
+Piece::~Piece()
+{
+}
+
+void Piece::setPlace(const std::string& place)
+{
+}
+
+void Piece::setType(const char type)
+{
+}
+
+void Piece::setHasMoved(const bool hasMoved)
+{
+}
+
+std::string Piece::getPlace() const
+{
+	return std::string();
+}
+
+bool Piece::getIsWhite() const
+{
+	return false;
+}
+
+char Piece::getType() const
+{
+	return 0;
+}
+
+bool Piece::gethasMoved() const
+{
+	return false;
+}
---
+++ Piece.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+class Piece {
+protected:
+	std::string _place;
+	char _type;
+	bool _hasMoved;
+public:
+	//basic object actions
+	Piece(const std::string& place, const char type);
+	virtual ~Piece();
+	//setters
+	void setPlace(const std::string& place);
+	void setType(const char type);
+	void setHasMoved(const bool hasMoved);
+	//getters
+	std::string getPlace() const;
+	bool getIsWhite() const;
+	char getType() const;
+	bool gethasMoved() const;
+	//pure virtual per piece functions
+	virtual void move(const std::string& dest) = 0;
+	virtual bool isValidMove(const std::string& dest) = 0;
+	virtual void eat(Piece& other) = 0;
+};
