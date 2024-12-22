6b000ecf30b3351c2d282ec3ddb4e8f09ec7c44b
unknown <yonatan.zohar44@gmail.com>
2024-12-22 13:34:44 +0200

    added basic board.cpp

---
+++ Board.cpp
@@ -0,0 +1,55 @@
+#include "Board.h"
+#include <iostream>
+
+//top left is [0][0] as it is wayyy easier to look at while running over the array
+#define startingBoard "RNBKQBNRPPPPPPPP################################pppppppprnbkqbnr"
+
+
+Board::Board(Piece* _pieces[BOARD_SIZE][BOARD_SIZE], const bool turn = 0)
+{
+	this->_turn = turn;
+
+	_pieces = new Piece*[BOARD_SIZE][BOARD_SIZE];
+
+	for (int i = 0; i < BOARD_SIZE; i++)
+	{
+		for (int j = 0; j < BOARD_SIZE; j++)
+		{
+			this->_pieces[i][j] = new Piece(indexToPlace(i,j),startingBoard[i + j]);
+		}
+	}
+}
+
+Board::~Board()
+{
+	for (int i = 0; i < BOARD_SIZE; i++)
+	{
+		for (int j = 0; j < BOARD_SIZE; j++)
+		{
+			delete this->_pieces[i][j];
+		}
+	}
+}
+
+void Board::printBoard() const
+{
+	for (int i = 0; i < BOARD_SIZE; i++)
+	{
+		for (int j = 0; j < BOARD_SIZE; j++)
+		{
+			std::cout << this->_pieces[i][j] << " ";
+		}
+		std::cout << '\n';
+	}
+}
+
+//turns an index [i, j] 
+std::string indexToPlace(int i, int j)
+{
+	std::string place = "";
+
+	place += (int('a') + j);
+	place += (8 - i);
+	
+	return place;
+}
--- Board.h
+++ Board.h
@@ -5,9 +5,10 @@
 class Board {
 protected:
 	Piece* _pieces[BOARD_SIZE][BOARD_SIZE];
+	bool _turn;
 public:
 	//basic class functions
-	Board(Piece* _pieces[BOARD_SIZE][BOARD_SIZE], const bool turn);
+	Board(Piece* _pieces[BOARD_SIZE][BOARD_SIZE], const bool turn = 0);
 	virtual ~Board();
 	void startGame();
 	void printBoard() const;
